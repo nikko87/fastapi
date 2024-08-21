64914bd3e325076e72740ab479ba2a09f38f0eea
Nikolas Castro <nikolascastro87@gmail.com>
2024-08-21 10:52:42 -0300

    refactor url

--- adapters/iris_adapter.py
+++ adapters/iris_adapter.py
@@ -5,4 +5,4 @@ from interfaces.integration_interface import IntegrationInterface
 
 class IrisAdapter(IntegrationInterface):
     def get_redirect_url(self, patient_data: dict[str, any]) -> str:
-        return "https://videocalldoutorsalva.irisemergencia.com/VideoCall/VideoCall.html?MasterId=38&idChamada=ZDNjM2RkNGUtNTE1My00MTIyLTk0NDEtYjQ2MGI4ZDU4ODA2"
+        return "https://videocalldoutorsalva.irisemergencia.com/VideoCall/VideoCall.html?MasterId=38&idChamada=ZGU1YjlkM2UtNGZkMi00MjIxLWIxMzYtNTk5Y2UyNzYwOWQ2"
