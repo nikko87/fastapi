5d3f2ad3d40693c71e8be31e25ef77696f2e3d3c
Nikolas Castro <nikolascastro87@gmail.com>
2024-08-22 11:13:57 -0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import sys
 
-from fastapi import FastAPI
+import httpx
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
@@ -29,6 +30,30 @@ logger = logging.getLogger(__name__)
 
 
 # test IRIS
+@app.get("/telemedicina/{user_id}")
+async def telemedicine_forward(user_id: str):
+    logger.info(f"Recebido request para o usuário {user_id}")
+
+    adapter = IrisAdapter()
+    use_case = GetRedirectUrl(adapter)
+    controller = IntegrationController(use_case)
+
+    forward_url = controller.get_redirect_url({"teste": "teste_url"})
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(forward_url)
+            print(response)
+    except httpx.HTTPStatusError as exc:
+        raise HTTPException(
+            status_code=exc.response.status_code, detail=exc.response.text)
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=str(exc))
+
+    return response.json()
+
+
+# test IRIS
 @app.get("/telemedicina/redirect/{user_id}")
 async def telemedicine_redirect(user_id: str):
     logger.info(f"Recebido request para o usuário {user_id}")
@@ -39,12 +64,13 @@ async def telemedicine_redirect(user_id: str):
 
     url_redirect = controller.get_redirect_url({"teste": "teste_url"})
 
-    logger.info(f"Sucesso. Usuário" f"{user_id}." f"redirecionado para {url_redirect}")
+    logger.info(f"Sucesso. Usuário" f"{
+                user_id}." f"redirecionado para {url_redirect}")
 
     return RedirectResponse(url=url_redirect)
 
 
-@app.post("/telemedicina/{user_id}")
+@app.post("/telemedicina/post/{user_id}")
 async def telemedicine_post(user_id: str):
     logger.info(f"Recebido request para o usuário {user_id}")
 
@@ -54,6 +80,7 @@ async def telemedicine_post(user_id: str):
 
     url_redirect = controller.get_redirect_url({"teste": "teste_url"})
 
-    logger.info(f"Sucesso. Usuário" f"{user_id}." f"redirecionado para {url_redirect}")
+    logger.info(f"Sucesso. Usuário" f"{
+                user_id}." f"redirecionado para {url_redirect}")
 
     return {"url": url_redirect}
